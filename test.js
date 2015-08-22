869b423d08c0e262108925fce5fcfe66ac1deb68
Joe Hitchens <joe@sleepless.com>
2015-08-22 16:02:48 -0700

    -

--- test.js
+++ test.js
@@ -2,43 +2,9 @@
 maws = require("./maws.js");
 require("sleepless");
 
-// maws.dbg = function(s) { log("MAWS: "+s) }
+maws.dbg = function(s) { log("MAWS: "+s) }
 
 seq = 0
-clients = {}
-
-who = function() {
-	var a = []
-	for(var k in clients) {
-		a.push(clients[k].name);
-	}
-	return a
-}
-
-
-bcast = function(m) {
-	for(var k in clients) {
-		log("::bcast:: "+k+" "+o2j(m))
-		var client = clients[k];
-		client.conn.send(m)
-	}
-}
-
-bcast_who = function() {
-	bcast({msg:'who', who:who()});
-}
-
-
-m_who = function(m) {
-	m.reply({who:who()})
-}
-
-m_chat = function(m, client) {
-	//var s = client.name+" says, \""+m.text+"\"";
-	m.name = client.name
-	bcast(m);
-}
-
 
 connect = function(req, cb_accept) {
 
@@ -47,35 +13,23 @@ connect = function(req, cb_accept) {
 	var cb_msg = function(m) {
 		log(name+": "+o2j(m))
 
-		var f = global["m_"+m.msg]
-		if(f) {
-			f(m, clients[name])
-			return
+		if(m.msg == "hello") {
+			m.reply({msg:"welcome", name:name})
+			conn.send({msg:"ping"}, function(r) {
+				log(o2j(r));
+			})
 		}
-
 	}
 
 	var cb_ctrl = function(s, xtra) {
 		log("[CTRL] "+name+": "+s+", ["+o2j(xtra)+"]")
-
 		if(s === "close") {
-			delete clients[name]
-			bcast_who();
-			bcast({msg:"depart", name:name});
 			return
 		}
 	}
 
 	conn = cb_accept(cb_msg, cb_ctrl)
-	clients[name] = {
-		conn: conn,
-		name: name,
-	}
 
-	conn.send({msg:"welcome to the maws test server"})
-
-	bcast_who();
-	bcast({msg:"arrive", name:name});
 }
 
 maws.listen( 12345, connect, "docroot")
